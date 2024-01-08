f6a152f3665987ab59da48aad27d39f3a343d462
Adarsh <adarshofficial.1607@gmail.com>
2024-01-08 19:46:50 +0530

    adding email

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,13 +5,31 @@ import { FaTwitter } from "react-icons/fa";
 import { IoMdColorPalette } from "react-icons/io";
 
 const Footer = ({ darkTheme, setDarkTheme }) => {
+
+  const sendEmail = () => {
+    // Replace 'your_email@example.com' with your actual email address
+    const emailAddress = 'adarshunofficial.1607@gmail.com';
+
+    // Replace 'Subject' and 'Body' with your desired subject and body text
+    const subject = 'Subject';
+    const body = 'Hello,';
+
+    // Create the mailto link
+    const mailtoLink = `mailto:${encodeURIComponent(emailAddress)}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+
+    // Open default email client with pre-filled email
+    window.location.href = mailtoLink;
+  };
+  
   return (
     <div className="w-full flex items-center justify-between" >
       <div className="flex items-center justify-center">
+        <a href="https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSGKZRNGrHvPhcBtmNVwXhhtwLCNfSmBhfRNcmdRPlPxrpfRwQDhbRFDskfkzbMFCCFBsbSM" target="_blank">
         <div className="flex justify-center items-center text-[#646669] hover:text-black dark:hover:text-white cursor-pointer transition-all ease-in-out duration-500">
-          <IoMail className='text-[15px]' />
+          <IoMail onClick={sendEmail} className='text-[15px]' />
           <h1 className="ml-[5px] text-[15px]">Contact</h1>
         </div>
+        </a>
 
         <div className="flex justify-center items-center ml-[15px] text-[#646669] hover:text-black dark:hover:text-white cursor-pointer transition-all ease-in-out duration-500">
           <MdContactSupport className='text-[15px]' />
